298383b2fae96dd9e0a9c13b8656911441c30007
Jakers <>
2021-11-16 15:23:23 -0800

    Update orcs.js

--- functions/orcs.js
+++ functions/orcs.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 exports.handler = function (event, context, callback) {
-  //let response = await fetch('https://etherorcs-spy.vercel.app/api/holders');
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
@@ -12,23 +11,20 @@ exports.handler = function (event, context, callback) {
     url += '/' + event.queryStringParameters.wallet;
   }
 
-  axios.get('https://etherorcs-spy.vercel.app/api/holders').then((resp) => {
-    callback(null, {
-      statusCode: 200,
-      headers,
-      body: JSON.stringify(resp.data),
+  axios
+    .get(url)
+    .then((resp) => {
+      callback(null, {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(resp.data),
+      });
+    })
+    .catch((err) => {
+      callback(null, {
+        statusCode: 500,
+        headers,
+        body: JSON.stringify(err),
+      });
     });
-  });
-
-  //   axios
-  //     .get('https://httpbin.org/get')
-  //     .then((res) => {
-  //       callback(null, { statusCode: 200, body: JSON.stringify(res) });
-  //     })
-  //     .catch(function (err) {
-  //       callback(null, {
-  //         statusCode: 200,
-  //         body: JSON.stringify({ err: err }),
-  //       });
-  //     });
 };
